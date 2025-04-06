026a952103d95094fd7af2e425e6e17246cdec72
Abdellah Massir <abdellahmassir6@gmail.com>
2025-04-06 18:14:50 +0100

    regler le probleme de caroussel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Title } from '@angular/platform-browser';
 import { ProjectsService } from '../_services/projects.service';
-import { Component, OnInit } from '@angular/core';
-import {Title} from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 
 @Component({
@@ -10,12 +11,18 @@ import { Project } from '../_models/Project';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
   featuredProject = {} as Project;
+  isBrowser: boolean;
 
-  constructor(private titleService : Title, private projectService: ProjectsService){
+  constructor(
+    private titleService: Title,
+    private projectService: ProjectsService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
     this.titleService.setTitle('Abdellah Massir - Home');
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
   ngOnInit(): void {
